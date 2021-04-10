86a7daf32e9eca2c019125efa1208a2da2d85a96
jonesyjava <jonesyjava@gmail.com>
2021-04-09 21:46:17 -0600

    Blogs Controller/Service/Repo/Models Complete

---
+++ Services/BlogsService.cs
@@ -0,0 +1,33 @@
+using System;
+using sharpblogger.Models;
+
+namespace Services
+{
+    public class BlogsService
+    {
+        internal object GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal object GetOneById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal Blog CreateBlog(Blog newBlog)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal Blog EditBlog(Blog editedBlog)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal object DeleteBlog(int id1, string id2)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Services/CommentsService.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Services
+{
+    public class CommentsService
+    {
+        internal object GetCommentsByBlogId(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
